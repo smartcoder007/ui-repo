c97f3537d760904781933ec9b95618ca08e33178
smartcoder007 <kareemmca@gmail.com>
2024-03-09 17:48:03 +0530

    Delete java-repo-2 directory

--- java-repo-2/src/main/java/corejava/factorypattern/Notification.java
+++
@@ -1,5 +0,0 @@
-package corejava.factorypattern;
-
-public interface Notification {
-    void notifyUser();
-}
--- java-repo-2/src/main/java/corejava/factorypattern/NotificationService.java
+++
@@ -1,9 +0,0 @@
-package corejava.factorypattern;
-
-public class NotificationService {
-    public static void main(String[] args)
-    {
-        Notification notification = NotificationFactory.createNotification("SMS");
-        notification.notifyUser();
-    }
-}
